01aebadb73e9d28b7a5524d9d2f782884a82d5b4
Jonas Voßemer <jonasvossemer@gmail.com>
2023-05-23 17:06:04 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 #plotly
 import plotly.express as px
 import plotly.graph_objects as go
 
 import streamlit as st
 
-st.title("MPG")
+st.title("My first streamlit app analyzing the MPG data")
 
 df = pd.read_csv("data/mpg.csv")
 
 # Basic set-up of the page:
+
 # First the checkbox to show the data frame
 if st.sidebar.checkbox('Show dataframe'):
     st.header("dataframe")
@@ -26,18 +28,18 @@ show_plot = st.sidebar.radio(
     label='Choose Plot type', options=['Matplotlib', 'Plotly'])
 
 st.header("Highway Fuel Efficiency")
-years = ["All"]+sorted(pd.unique(df['year']))
-year = st.sidebar.selectbox("choose a Year", years)   # Here the selection of the year.
+years = ["All"] + sorted(pd.unique(df['year']))
+year = st.sidebar.selectbox("Choose a year", years) # Here the selection of the year.
 car_classes = ['All'] + sorted(pd.unique(df['class']))
-car_class = st.sidebar.selectbox("choose a Class", car_classes)  # and the selection of the class.
+car_class = st.sidebar.selectbox("choose a Class", car_classes) # Here the selection of the class.
 
 show_means = st.sidebar.radio(
     label='Show Class Means', options=['Yes', 'No'])
 
 st.subheader(f'Fuel efficiency vs. engine displacement for {year}')
 
-
 # With these functions we wrangle the data and plot it.
+
 def mpg_mpl(year, car_class, show_means):
     fig, ax = plt.subplots()
     if year == 'All':
@@ -45,7 +47,7 @@ def mpg_mpl(year, car_class, show_means):
     else:
         group = df[df['year'] == year]
     if car_class != 'All':
-        st.text(f'plotting car class: {car_class}')
+        st.text(f'Plotting car class: {car_class}')
         group = group[group['class'] == car_class]
     group.plot('displ', 'hwy', marker='.', linestyle='', ms=12, alpha=0.5, ax=ax, legend=None)
     if show_means == "Yes":
@@ -57,7 +59,6 @@ def mpg_mpl(year, car_class, show_means):
     ax.set_ylim([10, 50])
     plt.close()
     return fig
-
 
 def mpg_plotly(year, car_class, show_means):
     if year == 'All':
@@ -73,11 +74,9 @@ def mpg_plotly(year, car_class, show_means):
         fig.add_trace(go.Scatter(x=group['displ'], y=group['hwy'], mode='markers', name=f'{year}_{car_class}',
                                  opacity=0.5, marker=dict(color="RoyalBlue")))
     return fig
-  
 
 if show_plot == 'Plotly':
     st.plotly_chart(mpg_plotly(year, car_class, show_means))
     
 else:
     st.pyplot(mpg_mpl(year, car_class, show_means))
-
